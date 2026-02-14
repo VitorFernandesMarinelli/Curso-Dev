c241b56e2e0a2c96cb2ab2cf92c4549b509b6811
VitorMarinelli <vitorFmarinelli@protonmail.com>
2026-02-14 15:56:30 -0300

    class 2

--- java script basico/aula1.js
+++
@@ -1,8 +0,0 @@
-//leaning the first function, console.log()
-
-console.log('Hello word');
-console.log(42);
-console.log(3.14);
-console.log("'test'");
-
-
--- java script basico/exercicio1.js
+++
@@ -1,3 +0,0 @@
-// Send a mensager with my name and time in moment to made this class
-
-console.log("My name is 'Vitor'. I leaning JavaScript at", 16 +":" +20,"PM");
